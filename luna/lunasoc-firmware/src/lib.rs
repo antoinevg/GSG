b2b2d039fbfef108dc3cc9942cba571146de627f
Antoine van Gelder <antoine@greatscottgadgets.com>
2023-01-13 15:50:30 +0200

    luna.firmware: Add error type

--- luna/lunasoc-firmware/src/lib.rs
+++ luna/lunasoc-firmware/src/lib.rs
@@ -1,4 +1,10 @@
+#![feature(error_in_core)]
+
 #![no_std]
+
+// - modules ------------------------------------------------------------------
+
+
 
 // - aliases ------------------------------------------------------------------
 
@@ -8,3 +14,21 @@ pub use lunasoc_pac as pac;
 // - constants ----------------------------------------------------------------
 
 pub const SYSTEM_CLOCK_FREQUENCY: u32 = 10_000_000;
+
+// - Error --------------------------------------------------------------------
+
+#[derive(Debug)]
+pub enum Error {
+    InvalidControlRequest,
+    Unknown,
+}
+
+impl core::fmt::Display for Error {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        core::fmt::Debug::fmt(&self, f)
+    }
+}
+
+impl core::error::Error for Error {}
+
+pub type Result<T> = core::result::Result<T, Error>;
