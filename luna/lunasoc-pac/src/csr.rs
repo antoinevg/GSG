d04d5736bfd1ba99c39639bdefbf79fe9bb0406b
Antoine van Gelder <antoine@greatscottgadgets.com>
2023-01-25 12:08:14 +0200

    luna.pac: ensure csr writes

--- luna/lunasoc-pac/src/csr.rs
+++ luna/lunasoc-pac/src/csr.rs
@@ -7,13 +7,17 @@ pub mod interrupt {
     use crate::Interrupt;
 
     pub unsafe fn enable(interrupt: Interrupt) {
-        let mask = crate::register::minerva::mim::read();
-        register::minerva::mim::write(mask | (1 << interrupt as usize))
+        let mask = register::minerva::mim::read();
+        let mask = mask | (1 << interrupt as usize);
+        register::minerva::mim::write(mask);
+        while register::minerva::mim::read() != mask {}
     }
 
     pub unsafe fn disable(interrupt: Interrupt) {
         let mask = register::minerva::mim::read();
-        register::minerva::mim::write(mask & !(1 << interrupt as usize))
+        let mask = mask & !(1 << interrupt as usize);
+        register::minerva::mim::write(mask);
+        while register::minerva::mim::read() != mask {}
     }
 
     pub unsafe fn pending(interrupt: Interrupt) -> bool {
@@ -21,7 +25,11 @@ pub mod interrupt {
         (pending & (1 << interrupt as usize)) != 0
     }
 
-    pub unsafe fn irqno() -> usize {
+    pub unsafe fn reg_mask() -> usize {
+        register::minerva::mim::read()
+    }
+
+    pub unsafe fn reg_pending() -> usize {
         register::minerva::mip::read()
     }
 }
