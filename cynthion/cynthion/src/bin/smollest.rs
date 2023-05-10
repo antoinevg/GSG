7971d5ef36367c2518395d6d626e1ed97ee669d1
Antoine van Gelder <antoine@greatscottgadgets.com>
2023-05-10 16:15:09 +0200

    checkpoint: smollest repro to date

--- cynthion/cynthion/src/bin/smollest.rs
+++ cynthion/cynthion/src/bin/smollest.rs
@@ -1,4 +1,4 @@
-#![allow(dead_code)]
+#![allow(dead_code, unused_imports, unused_mut, unused_variables)]
 
 #![no_std]
 #![no_main]
@@ -6,54 +6,51 @@
 use core::fmt::Write;
 use cynthion::pac;
 
-#[cfg(feature = "vexriscv")]
-#[riscv_rt::pre_init]
-unsafe fn pre_main() {
-    pac::cpu::vexriscv::flush_icache();
-    #[cfg(feature = "vexriscv_dcache")]
-    pac::cpu::vexriscv::flush_dcache();
+// - panic handler ------------------------------------------------------------
+
+#[no_mangle]
+#[panic_handler]
+fn panic(panic_info: &core::panic::PanicInfo) -> ! {
+    unsafe { core::ptr::write_volatile(IO_LEDS as *mut u32, 0b11_1100) };
+    loop { }
 }
+
+#[export_name = "ExceptionHandler"]
+fn custom_exception_handler(panic_info: &core::panic::PanicInfo) -> ! {
+    unsafe { core::ptr::write_volatile(IO_LEDS as *mut u32, 0b11_1110) };
+    loop { }
+}
+
+// - riscv_rt::main -----------------------------------------------------------
 
 #[riscv_rt::entry]
 fn main() -> ! {
-    let mut serial = Writer;
+    const MSG: &'static str = "Entering main loop.";
 
-    writeln!(serial, "Formatted output: 0x{:08x} addr", IO_LEDS).unwrap();
-    writeln!(serial, "Entering main loop.").unwrap();
+    unsafe { core::arch::asm!("nop", options(nomem, nostack)) };
+    unsafe { core::arch::asm!("nop", options(nomem, nostack)) };
+    unsafe { core::arch::asm!("nop", options(nomem, nostack)) };
+    unsafe { core::arch::asm!("nop", options(nomem, nostack)) };
+    for &b in MSG.as_bytes() { }
 
     let mut counter = 0;
     loop {
         unsafe { riscv::asm::delay(1_000_000) };
-        unsafe { core::ptr::write_volatile(IO_LEDS as *mut _, counter & 0b11_1111) };
+        unsafe { core::ptr::write_volatile(IO_LEDS as *mut u32, counter & 0b11_1111) };
         counter += 1;
     }
 }
 
-// - peripheral registers -----------------------------------------------------
+// - peripherals --------------------------------------------------------------
 
 const IO_BASE: usize = 0x8000_0000;
 const IO_LEDS: usize = IO_BASE + 0x0080;
 const IO_UART_TX_DATA: usize = IO_BASE + 0x0010;
 const IO_UART_TX_RDY: usize = IO_BASE + 0x0014;
 
-// - core::fmt::Write ---------------------------------------------------------
-
-#[inline(never)]
 fn uart_tx(s: &str) {
-    let tx_data = IO_UART_TX_DATA as *mut u32;
-    let tx_ready = IO_UART_TX_RDY as *mut u32;
-    for &c in s.as_bytes() {
-        while unsafe { core::ptr::read_volatile(tx_ready) } == 0 { }
-        unsafe { core::ptr::write_volatile(tx_data, c as u32 & 0b1111_1111) };
-    }
-}
-
-struct Writer;
-
-impl core::fmt::Write for Writer {
-    #[inline(never)]
-    fn write_str(&mut self, s: &str) -> core::fmt::Result {
-        uart_tx(s);
-        Ok(())
+    for b in s.bytes() {
+        while unsafe { core::ptr::read_volatile(IO_UART_TX_RDY as *mut u32) } == 0 { }
+        unsafe { core::ptr::write_volatile(IO_UART_TX_DATA as *mut u32, b as u32 & 0b1111_1111) };
     }
 }
